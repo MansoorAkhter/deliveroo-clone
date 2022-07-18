43e2f894ccfc37598cc029951bf6477455f2b315
Mansoor Akhter <mansoor21st@Gmail.com>
2022-07-18 21:56:28 +0500

    starting
    flex issues

---
+++ screens/HomeScreen.js
@@ -0,0 +1,40 @@
+import { View, Text, SafeAreaView, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { ChevronDownIcon } from "react-native-heroicons/outline";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    })
+  }, [])
+
+  return (
+    <SafeAreaView>
+      <Text className="text-green-600 mt-10 p-2">
+
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+
+          <Image source={{
+            uri: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/66acdb86006859.5d8c9386bd8f4.jpg"
+          }} className="h-7 w-7 bg-gray-300 p-4 rounded-full" />
+
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+
+            <Text className="font-bold text-xl">Current Location
+            <ChevronDownIcon size={20} color="#00ccbb"/>
+            </Text>
+          </View>
+
+        </View>
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
